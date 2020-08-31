52471eef5b970b0fcdcb949a63cbee4ff5b8372e
jbryzek <48593836+jbryzek@users.noreply.github.com>
2020-08-31 23:54:41 +0200

    app

---
+++ Hangman/Hangman/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Hangman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblsample = new System.Windows.Forms.Label();
+            this.btnSolution = new System.Windows.Forms.Button();
+            this.lblsample2 = new System.Windows.Forms.Label();
+            this.lblLine = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblsample
+            // 
+            this.lblsample.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblsample.ForeColor = System.Drawing.Color.Blue;
+            this.lblsample.Location = new System.Drawing.Point(12, 77);
+            this.lblsample.Name = "lblsample";
+            this.lblsample.Size = new System.Drawing.Size(40, 40);
+            this.lblsample.TabIndex = 0;
+            this.lblsample.Visible = false;
+            // 
+            // btnSolution
+            // 
+            this.btnSolution.Location = new System.Drawing.Point(274, 377);
+            this.btnSolution.Name = "btnSolution";
+            this.btnSolution.Size = new System.Drawing.Size(126, 52);
+            this.btnSolution.TabIndex = 1;
+            this.btnSolution.Text = "Solution";
+            this.btnSolution.UseVisualStyleBackColor = true;
+            this.btnSolution.Click += new System.EventHandler(this.btnSolution_Click);
+            // 
+            // lblsample2
+            // 
+            this.lblsample2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblsample2.ForeColor = System.Drawing.Color.Blue;
+            this.lblsample2.Location = new System.Drawing.Point(68, 283);
+            this.lblsample2.Name = "lblsample2";
+            this.lblsample2.Size = new System.Drawing.Size(81, 81);
+            this.lblsample2.TabIndex = 2;
+            this.lblsample2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblsample2.Visible = false;
+            // 
+            // lblLine
+            // 
+            this.lblLine.BackColor = System.Drawing.Color.Black;
+            this.lblLine.ForeColor = System.Drawing.Color.Blue;
+            this.lblLine.Location = new System.Drawing.Point(217, 283);
+            this.lblLine.Name = "lblLine";
+            this.lblLine.Size = new System.Drawing.Size(60, 8);
+            this.lblLine.TabIndex = 3;
+            this.lblLine.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 450);
+            this.Controls.Add(this.lblLine);
+            this.Controls.Add(this.lblsample2);
+            this.Controls.Add(this.btnSolution);
+            this.Controls.Add(this.lblsample);
+            this.Name = "Form1";
+            this.Text = "Hangman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblsample;
+        private System.Windows.Forms.Button btnSolution;
+        private System.Windows.Forms.Label lblsample2;
+        private System.Windows.Forms.Label lblLine;
+    }
+}
+
---
+++ Hangman/Hangman/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    public partial class Form1 : Form
+    {
+        const int MAX = 26;
+        const int MAX2 = 6;
+        Label[] Labels = new Label[MAX];
+        Label[] SolutionLabels = new Label[MAX2];
+        Label[] LineLabels = new Label[MAX2];
+        private Point p; //location of particular letter
+        private Point location; //real location
+        bool doDrag = false; //boolean value of dragging
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load the 2x13 array of labels
+            int lWidth = lblsample.Width;
+            int lHeight = lblsample.Height;
+            char tmp = 'A';
+
+            for (int i = 0; i < MAX; i++)
+            {
+                //properties
+                Labels[i] = new Label();
+                Labels[i].Left = 60 + i % 13 * lWidth;
+                Labels[i].Top = 50 + i / 13 * lHeight;
+                Labels[i].Text = tmp.ToString();
+                Labels[i].Font = lblsample.Font;
+                Labels[i].TextAlign = lblsample.TextAlign;
+                Labels[i].BorderStyle = lblsample.BorderStyle;
+                Labels[i].Size = lblsample.Size;
+                Labels[i].ForeColor = lblsample.ForeColor;
+                Labels[i].Name = i.ToString();
+                //events
+                Labels[i].MouseDown += new System.Windows.Forms.MouseEventHandler(letter_MouseDown);
+                Labels[i].MouseMove += new System.Windows.Forms.MouseEventHandler(letter_MouseMove);
+                Labels[i].MouseUp += new System.Windows.Forms.MouseEventHandler(letter_MouseUp);
+                tmp++;
+            }
+            this.Controls.AddRange(Labels); //add array in the form
+
+            lWidth = lblsample2.Width;
+            lHeight = lblsample2.Height;
+            int lineWidth = lblLine.Width;
+            int lineHeight = lblLine.Height;
+            for (int i = 0; i < MAX2; i++)
+            {
+                //properties
+                SolutionLabels[i] = new Label();
+                SolutionLabels[i].Left = 60 + i % 6 * lWidth+i*15;
+                SolutionLabels[i].Top = 150 + i / 6 * lHeight;
+                SolutionLabels[i].Text = "";
+                SolutionLabels[i].Font = lblsample2.Font;
+                SolutionLabels[i].TextAlign = lblsample2.TextAlign;
+                SolutionLabels[i].BorderStyle = lblsample.BorderStyle;
+                SolutionLabels[i].Size = lblsample2.Size;
+                SolutionLabels[i].ForeColor = lblsample2.ForeColor;
+                SolutionLabels[i].Name = i.ToString();
+
+                LineLabels[i] = new Label();
+                LineLabels[i].Left = 65 + i % 6 * lineWidth+ i*30;
+                LineLabels[i].Top = 205 + i / 6 * lineWidth;
+                LineLabels[i].BackColor = lblLine.BackColor;
+                LineLabels[i].Size = lblLine.Size;
+            }
+            this.Controls.AddRange(SolutionLabels); //add array in the form
+            this.Controls.AddRange(LineLabels); //add array in the form
+        }
+
+        private void rightLetter(Label letter)
+        {
+            if (letter.Text == "C")
+                SolutionLabels[0].Text = letter.Text;
+            else if (letter.Text == "Y")
+                SolutionLabels[1].Text = letter.Text;
+            else if (letter.Text == "P")
+                SolutionLabels[2].Text = letter.Text;
+            else if (letter.Text == "R")
+                SolutionLabels[3].Text = letter.Text;
+            else if (letter.Text == "U")
+                SolutionLabels[4].Text = letter.Text;
+            else if (letter.Text == "S")
+                SolutionLabels[5].Text = letter.Text;
+        }
+
+        private void solution()
+        {
+            int number = 0;
+            for(int i = 0; i < MAX2; i++)
+            {
+                if (SolutionLabels[i].Text != "")
+                    number++;
+            }
+            if (number == 6)
+            {
+                MessageBox.Show("Bravo - You Win", "Win", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                for (int i = 0; i < MAX2; i++)
+                {
+                    SolutionLabels[i].Text = "";
+                }
+            }
+        }
+        private void setLetter(Label letter)
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                if (letter.Left > SolutionLabels[i].Left && letter.Right < SolutionLabels[i].Right
+                    && letter.Top > SolutionLabels[i].Top && letter.Bottom < SolutionLabels[i].Bottom)
+                    rightLetter(letter);
+            }
+            letter.Location = location;
+            letter.ForeColor = Color.Blue;
+            solution();
+        }
+
+        private void btnSolution_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("The word is CYPRUS", "Solution", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void letter_MouseDown(object sender, MouseEventArgs e) //allow dragging
+        {
+            p = e.Location;
+            location = ((Label)sender).Location;
+            doDrag = true;
+            ((Label)sender).ForeColor = Color.Red;
+        }
+
+        private void letter_MouseUp(object sender, MouseEventArgs e) //end of dragging
+        {
+            doDrag = false;
+            setLetter((Label)sender);
+        }
+
+        private void letter_MouseMove(object sender, MouseEventArgs e) //dragging
+        {
+            if (doDrag)
+            {
+                ((Label)sender).Left = ((Label)sender).Left + e.X - p.X;
+                ((Label)sender).Top = ((Label)sender).Top + e.Y - p.Y;
+            }
+        }
+    }
+}
