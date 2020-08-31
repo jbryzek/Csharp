60ca24edffee23b53b0e39eeed676d3136ef19e9
jbryzek <48593836+jbryzek@users.noreply.github.com>
2020-08-31 23:46:28 +0200

    cellular automata 2

---
+++ gra/gra/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace gra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cbInitial = new System.Windows.Forms.ComboBox();
+            this.lblInitial = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.tbX = new System.Windows.Forms.TextBox();
+            this.lblx = new System.Windows.Forms.Label();
+            this.lbly = new System.Windows.Forms.Label();
+            this.tbY = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnSize = new System.Windows.Forms.Button();
+            this.btnInterval = new System.Windows.Forms.Button();
+            this.tbInterval = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbInitial
+            // 
+            this.cbInitial.FormattingEnabled = true;
+            this.cbInitial.Items.AddRange(new object[] {
+            "niezmienny",
+            "glider",
+            "reczna definicja",
+            "oscylator",
+            "losowy"});
+            this.cbInitial.Location = new System.Drawing.Point(402, 12);
+            this.cbInitial.Name = "cbInitial";
+            this.cbInitial.Size = new System.Drawing.Size(149, 28);
+            this.cbInitial.TabIndex = 1;
+            this.cbInitial.Text = "niezmienny";
+            // 
+            // lblInitial
+            // 
+            this.lblInitial.AutoSize = true;
+            this.lblInitial.Location = new System.Drawing.Point(262, 15);
+            this.lblInitial.Name = "lblInitial";
+            this.lblInitial.Size = new System.Drawing.Size(134, 20);
+            this.lblInitial.TabIndex = 2;
+            this.lblInitial.Text = "Stan poczatkowy:";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.Location = new System.Drawing.Point(557, 15);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(112, 20);
+            this.lblSize.TabIndex = 3;
+            this.lblSize.Text = "Rozmiar siatki:";
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(705, 12);
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(54, 26);
+            this.tbX.TabIndex = 4;
+            this.tbX.Text = "10";
+            // 
+            // lblx
+            // 
+            this.lblx.AutoSize = true;
+            this.lblx.Location = new System.Drawing.Point(675, 15);
+            this.lblx.Name = "lblx";
+            this.lblx.Size = new System.Drawing.Size(24, 20);
+            this.lblx.TabIndex = 5;
+            this.lblx.Text = "X:";
+            // 
+            // lbly
+            // 
+            this.lbly.AutoSize = true;
+            this.lbly.Location = new System.Drawing.Point(765, 15);
+            this.lbly.Name = "lbly";
+            this.lbly.Size = new System.Drawing.Size(24, 20);
+            this.lbly.TabIndex = 6;
+            this.lbly.Text = "Y:";
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(795, 12);
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(54, 26);
+            this.tbY.TabIndex = 7;
+            this.tbY.Text = "10";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(4, 1);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 54);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(91, 1);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(79, 54);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(176, 1);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(87, 54);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Czysc";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // dgv
+            // 
+            this.dgv.AllowUserToAddRows = false;
+            this.dgv.AllowUserToDeleteRows = false;
+            this.dgv.AllowUserToResizeColumns = false;
+            this.dgv.AllowUserToResizeRows = false;
+            this.dgv.ColumnHeadersHeight = 8;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dgv.ColumnHeadersVisible = false;
+            this.dgv.Location = new System.Drawing.Point(4, 70);
+            this.dgv.Name = "dgv";
+            this.dgv.RowHeadersVisible = false;
+            this.dgv.RowHeadersWidth = 8;
+            this.dgv.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dgv.RowTemplate.Height = 8;
+            this.dgv.Size = new System.Drawing.Size(724, 551);
+            this.dgv.TabIndex = 11;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnSize
+            // 
+            this.btnSize.Location = new System.Drawing.Point(755, 56);
+            this.btnSize.Name = "btnSize";
+            this.btnSize.Size = new System.Drawing.Size(126, 63);
+            this.btnSize.TabIndex = 12;
+            this.btnSize.Text = "Ustaw nowy rozmiar";
+            this.btnSize.UseVisualStyleBackColor = true;
+            this.btnSize.Click += new System.EventHandler(this.btnSize_Click);
+            // 
+            // btnInterval
+            // 
+            this.btnInterval.Location = new System.Drawing.Point(755, 183);
+            this.btnInterval.Name = "btnInterval";
+            this.btnInterval.Size = new System.Drawing.Size(126, 63);
+            this.btnInterval.TabIndex = 13;
+            this.btnInterval.Text = "Ustaw nowy interwal";
+            this.btnInterval.UseVisualStyleBackColor = true;
+            this.btnInterval.Click += new System.EventHandler(this.btnInterval_Click);
+            // 
+            // tbInterval
+            // 
+            this.tbInterval.Location = new System.Drawing.Point(795, 139);
+            this.tbInterval.Name = "tbInterval";
+            this.tbInterval.Size = new System.Drawing.Size(54, 26);
+            this.tbInterval.TabIndex = 14;
+            this.tbInterval.Text = "10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(893, 626);
+            this.Controls.Add(this.tbInterval);
+            this.Controls.Add(this.btnInterval);
+            this.Controls.Add(this.btnSize);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.lbly);
+            this.Controls.Add(this.lblx);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.lblInitial);
+            this.Controls.Add(this.cbInitial);
+            this.Name = "Form1";
+            this.Text = "The game of life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbInitial;
+        private System.Windows.Forms.Label lblInitial;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.TextBox tbX;
+        private System.Windows.Forms.Label lblx;
+        private System.Windows.Forms.Label lbly;
+        private System.Windows.Forms.TextBox tbY;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnSize;
+        private System.Windows.Forms.Button btnInterval;
+        private System.Windows.Forms.TextBox tbInterval;
+    }
+}
+
---
+++ gra/gra/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gra
+{
+    public partial class Form1 : Form
+    {
+        bool[,] board;
+        bool[,] newBoard;
+        bool afterStop = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (!afterStop)
+            {
+                alerty();
+
+                int sizeX = Convert.ToInt32(tbX.Text);
+                int sizeY = Convert.ToInt32(tbY.Text);
+
+                setInitialState(sizeX, sizeY);
+                drawing(sizeX, sizeY, 0);
+            }
+
+            afterStop = false;
+            timer1.Enabled = !timer1.Enabled;
+        }
+
+        private void drawing(int sizeX, int sizeY, int w) //draw board with colured cells
+        {
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    if (w > 0)
+                    {
+                        if (newBoard[i, j])
+                            dgv[i, j].Style.BackColor = Color.Black;
+                    }
+                    else
+                    {
+                        if (board[i, j])
+                            dgv[i, j].Style.BackColor = Color.Black;
+                    }
+                }
+            }
+        }
+
+        private void drawBoard(int sizeX, int sizeY) //draw clean board with setted width, height
+        {
+            dgv.GridColor = Color.Gray;
+            dgv.ColumnCount = sizeX;
+            dgv.RowCount = sizeY;
+
+            foreach (DataGridViewColumn c in dgv.Columns)
+                c.Width = dgv.Width / dgv.ColumnCount;
+            foreach (DataGridViewRow r in dgv.Rows)
+                r.Height = dgv.Height / dgv.RowCount;
+
+            for (int r = 0; r < dgv.RowCount; r++)
+            {
+                for (int c = 0; c < dgv.Columns.Count; c++)
+                {
+                    dgv[c, r].ReadOnly = true;
+                }
+            }
+            dgv[0, 0].Selected = false;
+        }
+
+        private void setInitialState(int sizeX, int sizeY) //only set initial values
+        {
+            int initialState = cbInitial.SelectedIndex;
+            if (initialState == 0)//constant
+            {
+                board[sizeX / 2, sizeY / 2] = true;
+                board[sizeX / 2 + 1, sizeY / 2] = true;
+                board[sizeX / 2 - 1, sizeY / 2 - 1] = true;
+                board[sizeX / 2 + 2, sizeY / 2 - 1] = true;
+                board[sizeX / 2, sizeY / 2 - 2] = true;
+                board[sizeX / 2 + 1, sizeY / 2 - 2] = true;
+            }
+            if (initialState == 1)//glider
+            {
+                board[sizeX / 2 + 1, sizeY / 2] = true;
+                board[sizeX / 2 - 1, sizeY / 2 - 1] = true;
+                board[sizeX / 2, sizeY / 2 - 1] = true;
+                board[sizeX / 2, sizeY / 2 - 2] = true;
+                board[sizeX / 2 + 1, sizeY / 2 - 2] = true;
+            }
+            if (initialState == 2)//manual definition
+            {
+                //only by cliking on the board
+            }
+            if (initialState == 3)//oscillator
+            {
+                board[sizeX / 2, sizeY / 2] = true;
+                board[sizeX / 2, sizeY / 2 + 1] = true;
+                board[sizeX / 2, sizeY / 2 - 1] = true;
+            }
+            if (initialState == 4)//random
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < sizeX; i++)
+                {
+                    for (int j = 0; j < sizeY; j++)
+                    {
+                        board[i, j] = Convert.ToBoolean(rnd.Next(2));
+                    }
+                }
+                timer1.Interval = 3000;
+                tbInterval.Text = "2000";
+            }
+        }
+
+        private void ZeroTheBoard(int sizeX, int sizeY, int w)
+        {
+            for (int i = 0; i < sizeY; i++)
+            {
+                for (int j = 0; j < sizeX; j++)
+                {
+                    if (w > 0)
+                        newBoard[i, j] = false;
+                    else
+                        board[i, j] = false;
+                }
+            }
+        }
+
+        private void alerty()
+        {
+            if (tbX.Text == "")
+            {
+                MessageBox.Show("You have to write width", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (tbY.Text == "")
+            {
+                MessageBox.Show("You have to write height", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (cbInitial.Text == "")
+            {
+                MessageBox.Show("You have to choose initial state", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (tbInterval.Text == "")
+            {
+                MessageBox.Show("You have to write interval", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgv[e.ColumnIndex, e.RowIndex].Style.BackColor != Color.Black)
+            {
+                dgv[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Black;
+                board[e.ColumnIndex, e.RowIndex] = true;
+
+            }
+            else
+            {
+                dgv[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.White;
+                board[e.ColumnIndex, e.RowIndex] = false;
+            }
+            dgv[e.ColumnIndex, e.RowIndex].Selected = false;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            int sizeX = Convert.ToInt32(tbX.Text);
+            int sizeY = Convert.ToInt32(tbY.Text);
+            ZeroTheBoard(sizeX, sizeY, 0);
+            ZeroTheBoard(sizeX, sizeY, 1);
+            dgv.Rows.Clear();
+            drawBoard(sizeX, sizeY);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int sizeX = Convert.ToInt32(tbX.Text);
+            int sizeY = Convert.ToInt32(tbY.Text);
+            board = new bool[sizeX, sizeY];
+            newBoard = new bool[sizeX, sizeY];
+            ZeroTheBoard(sizeX, sizeY, 0);
+            drawBoard(sizeX, sizeY);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int sizeX = Convert.ToInt32(tbX.Text);
+            int sizeY = Convert.ToInt32(tbY.Text);
+            ZeroTheBoard(sizeX, sizeY, 1);
+
+            int numberOfNeighbours = 0;
+
+            for (int i = 0; i < sizeX; i++)
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    if (i == 0 && j > 0 && board[sizeX - 1, j - 1])
+                        numberOfNeighbours++;
+                    if (board[sizeX - 1, j] && i == 0)
+                        numberOfNeighbours++;
+                    if (i == 0 && j < sizeY - 1 && board[sizeX - 1, j + 1])
+                        numberOfNeighbours++;
+
+                    if (board[sizeX - 1, sizeY - 1] && i == 0 && j == 0)
+                        numberOfNeighbours++;
+
+                    if (board[i, sizeY - 1] && j == 0)
+                        numberOfNeighbours++;
+                    if (j == 0 && i < sizeX - 1 && board[i + 1, sizeY - 1])
+                        numberOfNeighbours++;
+                    if (i > 0 && j == 0 && board[i - 1, sizeY - 1])
+                        numberOfNeighbours++;
+
+                    if (i > 0 && j > 0 && board[i - 1, j - 1])
+                        numberOfNeighbours++;
+                    if (j > 0 && board[i, j - 1])
+                        numberOfNeighbours++;
+                    if (j > 0 && i < sizeX - 1 && board[i + 1, j - 1])
+                        numberOfNeighbours++;
+                    if (i > 0 && board[i - 1, j])
+                        numberOfNeighbours++;
+                    if (i < sizeX - 1 && board[i + 1, j])
+                        numberOfNeighbours++;
+                    if (i > 0 && j < sizeY - 1 && board[i - 1, j + 1])
+                        numberOfNeighbours++;
+                    if (j < sizeY - 1 && board[i, j + 1])
+                        numberOfNeighbours++;
+                    if (i < sizeX - 1 && j < sizeY - 1 && board[i + 1, j + 1])
+                        numberOfNeighbours++;
+
+                    if (i == sizeX - 1 && j > 0 && board[0, j - 1])
+                        numberOfNeighbours++;
+                    if (board[0, j] && i == sizeX - 1)
+                        numberOfNeighbours++;
+                    if (i == sizeX - 1 && j < sizeY - 1 && board[0, j + 1])
+                        numberOfNeighbours++;
+
+                    if (i == sizeX - 1 && j == sizeY - 1 && board[0, 0])
+                        numberOfNeighbours++;
+
+                    if (i > 0 && j == sizeY - 1 && board[i - 1, 0])
+                        numberOfNeighbours++;
+                    if (board[i, 0] && j == sizeY - 1)
+                        numberOfNeighbours++;
+                    if (i < sizeX - 1 && j == sizeY - 1 && board[i + 1, 0])
+                        numberOfNeighbours++;
+                    
+                    if (board[i, j] && (numberOfNeighbours == 2 || numberOfNeighbours == 3)) //rules
+                        newBoard[i, j] = true;
+                    if (!board[i, j] && numberOfNeighbours == 3)
+                        newBoard[i, j] = true;
+                    numberOfNeighbours = 0;
+                }
+            }
+            dgv.Rows.Clear();
+            drawBoard(sizeX, sizeY);
+            drawing(sizeX, sizeY, 1);
+            for (int i = 0; i < sizeX; i++) //assign new table to the old one
+            {
+                for (int j = 0; j < sizeY; j++)
+                {
+                    board[i, j] = newBoard[i, j];
+                }
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = !timer1.Enabled;
+            afterStop = true;
+        }
+
+        private void btnSize_Click(object sender, EventArgs e)
+        {
+            Form1_Load(sender, e);
+        }
+
+        private void btnInterval_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(tbInterval.Text);
+        }
+    }
+}
