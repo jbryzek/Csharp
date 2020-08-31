c3750c0204e75cb60f9a8c5195adfe1c02690708
jbryzek <48593836+jbryzek@users.noreply.github.com>
2020-08-31 23:37:31 +0200

    app

---
+++ Puzzle app/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace JustynaBryzekUNIC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.levelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.beginnerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.advanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.redToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.greenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otherToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lblMove = new System.Windows.Forms.Label();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.example = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(462, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.levelToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(74, 29);
+            this.gameToolStripMenuItem.Text = "&Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(200, 34);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // levelToolStripMenuItem
+            // 
+            this.levelToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.beginnerToolStripMenuItem,
+            this.advanceToolStripMenuItem});
+            this.levelToolStripMenuItem.Name = "levelToolStripMenuItem";
+            this.levelToolStripMenuItem.Size = new System.Drawing.Size(200, 34);
+            this.levelToolStripMenuItem.Text = "Level";
+            // 
+            // beginnerToolStripMenuItem
+            // 
+            this.beginnerToolStripMenuItem.Checked = true;
+            this.beginnerToolStripMenuItem.CheckOnClick = true;
+            this.beginnerToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.beginnerToolStripMenuItem.Name = "beginnerToolStripMenuItem";
+            this.beginnerToolStripMenuItem.Size = new System.Drawing.Size(183, 34);
+            this.beginnerToolStripMenuItem.Text = "Beginner";
+            this.beginnerToolStripMenuItem.Click += new System.EventHandler(this.beginnerToolStripMenuItem_Click);
+            // 
+            // advanceToolStripMenuItem
+            // 
+            this.advanceToolStripMenuItem.CheckOnClick = true;
+            this.advanceToolStripMenuItem.Name = "advanceToolStripMenuItem";
+            this.advanceToolStripMenuItem.Size = new System.Drawing.Size(183, 34);
+            this.advanceToolStripMenuItem.Text = "Advance";
+            this.advanceToolStripMenuItem.Click += new System.EventHandler(this.advanceToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(200, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.redToolStripMenuItem,
+            this.greenToolStripMenuItem,
+            this.blueToolStripMenuItem,
+            this.otherToolStripMenuItem});
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(71, 29);
+            this.colorToolStripMenuItem.Text = "&Color";
+            // 
+            // redToolStripMenuItem
+            // 
+            this.redToolStripMenuItem.Name = "redToolStripMenuItem";
+            this.redToolStripMenuItem.Size = new System.Drawing.Size(160, 34);
+            this.redToolStripMenuItem.Text = "Red";
+            this.redToolStripMenuItem.Click += new System.EventHandler(this.redToolStripMenuItem_Click);
+            // 
+            // greenToolStripMenuItem
+            // 
+            this.greenToolStripMenuItem.Name = "greenToolStripMenuItem";
+            this.greenToolStripMenuItem.Size = new System.Drawing.Size(160, 34);
+            this.greenToolStripMenuItem.Text = "Green";
+            this.greenToolStripMenuItem.Click += new System.EventHandler(this.greenToolStripMenuItem_Click);
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(160, 34);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // otherToolStripMenuItem
+            // 
+            this.otherToolStripMenuItem.Name = "otherToolStripMenuItem";
+            this.otherToolStripMenuItem.Size = new System.Drawing.Size(160, 34);
+            this.otherToolStripMenuItem.Text = "Other";
+            this.otherToolStripMenuItem.Click += new System.EventHandler(this.otherToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(78, 29);
+            this.aboutToolStripMenuItem.Text = "&About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // lblMove
+            // 
+            this.lblMove.Location = new System.Drawing.Point(171, 394);
+            this.lblMove.Name = "lblMove";
+            this.lblMove.Size = new System.Drawing.Size(131, 23);
+            this.lblMove.TabIndex = 1;
+            this.lblMove.Text = "Move #";
+            this.lblMove.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(171, 420);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(131, 53);
+            this.btnNewGame.TabIndex = 2;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // example
+            // 
+            this.example.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.example.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.example.ForeColor = System.Drawing.Color.Red;
+            this.example.Location = new System.Drawing.Point(68, 55);
+            this.example.Name = "example";
+            this.example.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.example.Size = new System.Drawing.Size(100, 88);
+            this.example.TabIndex = 3;
+            this.example.Text = "A";
+            this.example.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.example.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 485);
+            this.Controls.Add(this.example);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.lblMove);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Puzzle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem levelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem beginnerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem advanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem redToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem greenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otherToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label lblMove;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label example;
+    }
+}
+
---
+++ Puzzle app/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JustynaBryzekUNIC
+{
+    public partial class Form1 : Form
+    {
+        const int MAX = 9;
+        Label[] Labels = new Label[MAX];
+        int numberOfMove;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void beginnerToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            advanceToolStripMenuItem.Checked = false;
+        }
+
+        private void advanceToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            beginnerToolStripMenuItem.Checked = false;
+        }
+
+        private void redToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void greenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Blue;
+        }
+
+        private void otherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            colorDialog1.Color = this.BackColor;
+            colorDialog1.ShowDialog();
+            this.BackColor = colorDialog1.Color;
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Justyna Bryzek", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Solved1()
+        {
+            for (int i=0;i<8;i++)
+            {
+                if (!(Labels[i].Text == (i+1).ToString()))
+                    return;
+            }
+            msgboxAppear();
+        }
+
+        private void Solved2()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if (!(Labels[i].Text == (i + 1).ToString()))
+                    return;
+            }
+            if (!(Labels[5].Text == "4"))
+                return;
+            if (!(Labels[8].Text == "5"))
+                return;
+            if (!(Labels[7].Text == "6"))
+                return;
+            if (!(Labels[6].Text == "7"))
+                return;
+            if (!(Labels[3].Text == "8"))
+                return;
+            msgboxAppear();
+        }
+
+        private void msgboxAppear()
+        {
+            //if solved -> message box appear
+            DialogResult result;
+            result = MessageBox.Show("Congratulations - You Win!", "Winning info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (result == DialogResult.OK)
+                NewGame();
+        }
+
+        private void btnNewGame_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+
+        private void NewGame()
+        {
+            numberOfMove = 0;
+            lblMove.Text = numberOfMove.ToString();
+            //random numbers
+            Random random = new Random();
+            int[] tabtemp = new int[9];
+            int temp = 0;
+            for (int i = 0; i < 9; i++)
+                tabtemp[i] = -1;
+            foreach (Label label in Labels)
+            {
+                int k;
+                do
+                {
+                    k = random.Next(MAX);
+                } while (tabtemp.Contains(k));
+
+                tabtemp[temp] = k;
+                temp++;
+                label.Text = k.ToString();
+            }
+            //label with zero make as empty label
+            foreach(Label a in Labels) 
+            {
+                if (a.Text == "0")
+                    a.Text = "";
+            }
+        }
+
+        private void Labels_ClickHandler(object sender, EventArgs e)
+        {
+            if (((Label)sender).Text == "") { return; } //do nothing if u click on empty label
+            int index=-1;
+            for(int i = 0; i < 9; i++) //finding clicked label
+            {
+                if (Labels[i] == (Label)sender)
+                {
+                    index = i;
+                }
+            }
+            neighbourZero(index);
+        }
+
+        private void neighbourZero(int index)
+        {
+            //check if selected label is neighbour to empty label
+            int indexZero = -1;
+            for (int i = 0; i < 9; i++) //finding where is empty label (zero label)
+            {
+                if (Labels[i].Text == "")
+                {
+                    indexZero = i;
+                }
+            }
+            if (index - 3 == indexZero || index + 3 == indexZero)
+            {
+                swap(index, indexZero);
+                numberOfMove++;
+            }
+            if (index - 1 == indexZero || index+1 == indexZero) //check if buttons aren't on the edgeds
+            {
+                if (index == 2 && indexZero == 3)
+                    return;
+                if (index == 5 && indexZero == 6)
+                    return;
+                if (index == 3 && indexZero == 2)
+                    return;
+                if (index == 6 && indexZero == 5)
+                    return;
+                swap(index, indexZero);
+                numberOfMove++;
+            }
+            lblMove.Text = numberOfMove.ToString();
+            Solved1(); //check one solution
+            Solved2(); //check other solution
+        }
+
+        private void swap(int x, int y)
+        {
+            //swap values of the labels
+            string temp = Labels[x].Text;
+            Labels[x].Text = Labels[y].Text;
+            Labels[y].Text = temp;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //load the 3x3 array of labels
+            int lWidth = example.Width;
+            int lHeight = example.Width;
+
+            for (int i = 0; i < MAX; i++)
+            {
+                //properties
+                Labels[i] = new Label();
+                Labels[i].Left = 50 + i % 3 * lWidth;
+                Labels[i].Top = 50 + i / 3 * lHeight;
+                Labels[i].Text = "";
+                Labels[i].Font = example.Font;
+                Labels[i].TextAlign = example.TextAlign;
+                Labels[i].BorderStyle = example.BorderStyle;
+                Labels[i].Size = example.Size;
+                Labels[i].ForeColor = example.ForeColor;
+                Labels[i].Name = i.ToString();
+                //events
+                Labels[i].Click += new System.EventHandler(Labels_ClickHandler);
+            }
+            this.Controls.AddRange(Labels); //add array in the form
+        }
+    }
+}
